c3ae9a152c944adf81a9526b94f0cb527df9c1a6
kevin fricovsky <kevin@montylounge.com>
2009-09-05 14:30:04 -0400

    updated to follow standards

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 from distutils.core import setup
- 
+
 setup(
     name='django-sugar',
     version='0.1',
+    license='BSD',
     description='Curated collection of all the sweet Django helpers/utilities \
         developers create, and sometimes recreate too often.',
     author='Kevin Fricovsky',
     author_email='kfricovsky@gmail.com',
+    license='BSD',
     url='http://github.com/montylounge/django-sugar/tree',
-    packages=['sugar'],
+    packages=[
+        'sugar',
+        'sugar.admin',
+        'sugar.cache',
+        'sugar.forms',
+        'sugar.middleware',
+        'sugar.templatetags',
+        'sugar.views',
+        'sugar.widgets',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
 )
- 
