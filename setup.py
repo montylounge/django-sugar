26bb6a8ab966707c53c817c9b16dbb0dee54d8e4
kevin fricovsky <kevin@montylounge.com>
2009-09-07 23:33:23 -0400

    removed dupe license reference

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 
 setup(
     name='django-sugar',
-    version='0.1',
+    version='0.2',
     license='BSD',
     description='Curated collection of all the sweet Django helpers/utilities \
         developers create, and sometimes recreate too often.',
     author='Kevin Fricovsky',
     author_email='kfricovsky@gmail.com',
-    license='BSD',
     url='http://github.com/montylounge/django-sugar/tree',
     packages=[
         'sugar',
@@ -21,7 +20,7 @@ setup(
         'sugar.widgets',
     ],
     classifiers=[
-        'Development Status :: 1 - Alpha',
+        'Development Status :: 2 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -30,3 +29,4 @@ setup(
         'Framework :: Django',
     ],
 )
+
