18985f7a8cf584cc6735eb577784314ddc7346e6
kevin fricovsky <kevin@montylounge.com>
2010-03-09 08:23:13 -0500

    updated import error

--- sugar/views/decorators.py
+++ sugar/views/decorators.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from lib.http import JsonResponse
+from sugar.views.json import JsonResponse
 
 def render_to(template):
     """
